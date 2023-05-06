be004457bd08972fb6c93343780b39b4bdb969af
SmigorX <94678003+SmigorX@users.noreply.github.com>
2023-05-06 19:23:01 +0200

    Changing how the data is processed in ContractFetcher.py

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
-const { spawn } = require('child_process');
-
 
 let mainWindow;
 
@@ -24,41 +22,18 @@ function createWindow() {
   });
 }
 
-function readFile(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        const dataArray = data.split(', ');
-        resolve(dataArray);
-      }
-    });
+ipcMain.on('read-file', (event, filePath) => {
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      event.reply('read-file-reply', err.message);
+    } else {
+      event.reply('read-file-reply', data);
+    }
   });
-}
-
-ipcMain.on('read-file', async (event, filePath) => {
-  try {
-    const dataArray = await readFile(filePath);
-    event.reply('read-file-reply', dataArray);
-  } catch (err) {
-    event.reply('read-file-reply', err.message);
-  }
 });
 
 app.whenReady().then(() => {
   createWindow();
-});
-
-
-const pythonProcess = spawn('python', ['./programs/App.py']);
-
-pythonProcess.stdout.on('data', (data) => {
-  console.log(`Python stdout: ${data}`);
-});
-
-pythonProcess.stderr.on('data', (data) => {
-  console.error(`Python stderr: ${data}`);
 });
 
 
